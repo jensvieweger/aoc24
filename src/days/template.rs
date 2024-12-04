5064819b4e230b094782aa2e1a09b1a295458a8f
Jens Vieweger <jensvieweger@users.noreply.github.com>
2024-12-04 17:01:20 +0100

    improved template a bit

--- src/days/template.rs
+++ src/days/template.rs
@@ -1,3 +1,12 @@
+fn parse_data(data: &Vec<String>) -> Option<Vec<u32>> {
+    let mut result:Vec<u32> = Vec::new();
+
+    for line in data {
+        result.push(1);
+    }
+    Some(result)
+}
+
 pub fn get_day(data: &Vec<String>) -> Option<u32> {
     None
 }
@@ -14,6 +23,17 @@ mod tests {
         data.push("bbbb".to_string());
 
         data
+    }
+
+    #[test]
+    fn test_parse_data() {
+        let data = fill_input();
+
+        let parsed_data = parse_data(&data);
+
+        assert_eq!(parsed_data.is_none(), false);
+
+        let final_data=parsed_data.unwrap();
     }
 
     #[test]
